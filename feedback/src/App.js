8098ca6a6ea55d699e1bcfaf3c803cfe98687dd0
syd333 <sydmiranda@icloud.com>
2022-07-26 15:03:55 -0400

    dummy card component

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header />
-      <div className="container">APP</div>
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
     </>
   );
 }
