01be923f04e41a563175cc7e541186eeb1736967
syd333 <sydmiranda@icloud.com>
2022-07-26 15:33:24 -0400

    stats + average

--- feedback/src/components/FeedbackItem.js
+++ feedback/src/components/FeedbackItem.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { FaTimes, FaEdit } from "react-icons/fa";
 import Card from "./shared/Card"
+import PropTypes from 'prop-types'
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <>
-      <Card>
+      <Card reverse>
         <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className="close">
+          <FaTimes color="purple" />
+        </button>
+        <button className="edit">
+          <FaEdit color="purple" />
+        </button>
         <div className="text-display">{item.text}</div>
       </Card>
     </>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
--- feedback/src/components/FeedbackList.js
+++ feedback/src/components/FeedbackList.js
@@ -1,14 +1,14 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     <p>No Feedback yet!</p>;
   }
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
